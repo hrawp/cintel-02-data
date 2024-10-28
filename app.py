30a3e35df8f02c4956e8b7280fd5f92f3ec5abcf
hrawp <epiihrawp@gmail.com>
2024-10-28 17:12:00 -0500

    finished sidebar app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ with ui.sidebar():
     ui.h2("Sidebar")
 #   pass in a string argument (in quotes) to set the header text to "Sidebar"
 
-    ui.input_selectize("selected_attribute","penguin's characteristics",
+    ui.input_selectize("selected_attribute","Penguin's Characteristics",
                       ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]) # to create a dropdown input to choose a column
 #   pass in three arguments:
 #   the name of the input (in quotes), e.g., "selected_attribute"
@@ -33,7 +33,7 @@ with ui.sidebar():
 #   a list of options for the input (in square brackets) 
 #   e.g. ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
 
-    ui.input_numeric("plotly_bin_count", "fidelity of columns",0) #to create a numeric input for the number of Plotly histogram bins
+    ui.input_numeric("plotly_bin_count", "Fidelity of Columns",0) #to create a numeric input for the number of Plotly histogram bins
 #   pass in two arguments:
 #   the name of the input (in quotes), e.g. "plotly_bin_count"
 #   the label for the input (in quotes)
@@ -46,11 +46,19 @@ with ui.sidebar():
 #   the maximum value for the input (as an integer)
 #   the default value for the input (as an integer)
 
-    ui.input_checkbox_group("selected_species_list","species",
-                           ["Adelie", "Gentoo", "Chinstrap"]) #to create a checkbox group input to filter the species
+    ui.input_checkbox_group("selected_species_list","Species",
+                           ["Adelie", "Gentoo", "Chinstrap"],selected=["Gentoo"],inline="True") #to create a checkbox group input to filter the species
 #   pass in five arguments:
 #   the name of the input (in quotes), e.g.  "selected_species_list"
 #   the label for the input (in quotes)
 #   a list of options for the input (in square brackets) as ["Adelie", "Gentoo", "Chinstrap"]
 #   a keyword argument selected= a list of selected options for the input (in square brackets)
-#   a keyword argument inline= a Boolean value (True or False) as you like
+#   a keyword argument inline= a Boolean value (True or False) as you
+    ui.hr() #to add a horizontal rule to the sidebar
+    ui.a("GitHub",href="https://github.com/hrawp/cintel-02-data",target= "_blank") #to add a hyperlink to the sidebar
+#   pass in two arguments:
+#   the text for the hyperlink (in quotes), e.g. "GitHub"
+#   a keyword argument href= the URL for the hyperlink (in quotes), e.g. your GitHub repo URL
+#   a keyword argument target= "_blank" to open the link in a new tab
+
+# When passing in multiple arguments to a function, separate them with commas.
