7af78d880b7623ab3269cfbd7ed918799eae1764
hrawp <epiihrawp@gmail.com>
2024-10-28 16:50:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,40 @@ with ui.layout_columns():
         return px.histogram(px.data.tips(), y="total_bill")
 
 # Add a Shiny UI sidebar for user interaction
-with ui.sidebar(open=True): # Use the ui.sidebar() function to create a sidebar
+with ui.sidebar():
 # Set the open parameter to "open" to make the sidebar open by default
 # Use a with block to add content to the sidebar
 
 # Use the ui.h2() function to add a 2nd level header to the sidebar
     ui.h2("Sidebar")
 #   pass in a string argument (in quotes) to set the header text to "Sidebar"
+
+    ui.input_selectize("selected_attribute","penguin's characteristics",
+                      ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]) # to create a dropdown input to choose a column
+#   pass in three arguments:
+#   the name of the input (in quotes), e.g., "selected_attribute"
+#   the label for the input (in quotes)
+#   a list of options for the input (in square brackets) 
+#   e.g. ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
+
+    ui.input_numeric("plotly_bin_count", "fidelity of columns",0) #to create a numeric input for the number of Plotly histogram bins
+#   pass in two arguments:
+#   the name of the input (in quotes), e.g. "plotly_bin_count"
+#   the label for the input (in quotes)
+
+    ui.input_slider("seaborn_bin_count","Choose number of bars",0,100,20) # to create a slider input for the number of Seaborn bins
+#   pass in four arguments:
+#   the name of the input (in quotes), e.g. "seaborn_bin_count"
+#   the label for the input (in quotes)
+#   the minimum value for the input (as an integer)
+#   the maximum value for the input (as an integer)
+#   the default value for the input (as an integer)
+
+    ui.input_checkbox_group("selected_species_list","species",
+                           ["Adelie", "Gentoo", "Chinstrap"]) #to create a checkbox group input to filter the species
+#   pass in five arguments:
+#   the name of the input (in quotes), e.g.  "selected_species_list"
+#   the label for the input (in quotes)
+#   a list of options for the input (in square brackets) as ["Adelie", "Gentoo", "Chinstrap"]
+#   a keyword argument selected= a list of selected options for the input (in square brackets)
+#   a keyword argument inline= a Boolean value (True or False) as you like
